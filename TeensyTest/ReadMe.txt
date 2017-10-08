af001b09d7a2049df7dbbd817a445270eefc8c64
William31415 <WAL@williams-mbp-4>
2017-10-07 22:49:53 -0500

    Initial Commit

---
+++ TeensyTest/ReadMe.txt
@@ -0,0 +1,26 @@
+
+  TeensyTest
+  Project
+  ----------------------------------
+  Developed with embedXcode
+
+  Project TeensyTest
+  Created by William Lies on 10/7/17
+  Copyright © 2017 William Lies
+  Licence <#licence#>
+
+
+
+  References
+  ----------------------------------
+
+
+
+  embedXcode
+  embedXcode+
+  ----------------------------------
+  Embedded Computing on Xcode
+  Copyright © Rei VILO, 2010-2017
+  All rights reserved
+  http://embedXcode.weebly.com
+
---
+++ TeensyTest/TeensyTest.ino
@@ -0,0 +1,64 @@
+//
+// TeensyTest
+//
+// 
+// Developed with [embedXcode](http://embedXcode.weebly.com)
+//
+// Author 		William Lies
+// 				William Lies
+//
+// Date			10/7/17 10:49 PM
+// Version		<#version#>
+//
+// Copyright	© William Lies, 2017
+// Licence		<#licence#>
+//
+// See         ReadMe.txt for references
+//
+
+
+// Core library for code-sense - IDE-based
+#if defined(ENERGIA) // LaunchPad specific
+#include "Energia.h"
+#elif defined(TEENSYDUINO) // Teensy specific
+#include "Arduino.h"
+#elif defined(ESP8266) // ESP8266 specific
+#include "Arduino.h"
+#elif defined(ARDUINO) // Arduino 1.8 specific
+#include "Arduino.h"
+#else // error
+#error Platform not defined
+#endif // end IDE
+
+// Set parameters
+
+
+// Include application, user and local libraries
+
+
+// Define structures and classes
+
+
+// Define variables and constants
+
+
+// Prototypes
+
+
+// Utilities
+
+
+// Functions
+
+
+// Add setup code
+void setup()
+{
+    ;
+}
+
+// Add loop code
+void loop()
+{
+    ;
+}
